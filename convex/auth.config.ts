03e9d28a150648749801c9e47881bf8ca03ac1d2
leadond <leadond@gmail.com>
2026-01-28 15:19:54 -0600

    fix: include production Clerk issuer in Convex auth config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,6 +1,12 @@
 export default {
     providers: [
         {
+            // Production Domain
+            domain: "https://clerk.myvillage.devapphero.com",
+            applicationID: "convex",
+        },
+        {
+            // Development Domain
             domain: "https://informed-tapir-68.clerk.accounts.dev",
             applicationID: "convex",
         },
